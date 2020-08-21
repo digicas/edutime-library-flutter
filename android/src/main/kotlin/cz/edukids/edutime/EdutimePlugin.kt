97c426569c0d706cbc7e94c138eb73116b4d5f55
Radek Svarz <radek.svarz@gmail.com>
2020-08-21 18:18:13 +0200

    flutter plugin base commit

---
+++ android/src/main/kotlin/cz/edukids/edutime/EdutimePlugin.kt
@@ -0,0 +1,40 @@
+package cz.edukids.edutime
+
+import androidx.annotation.NonNull
+import cz.edukids.sdk.EduTimeSdkInstance
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry.Registrar
+
+/** EdutimePlugin */
+class EdutimePlugin: FlutterPlugin, MethodCallHandler {
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  private lateinit var channel : MethodChannel
+
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "edutime")
+    channel.setMethodCallHandler(this)
+  }
+
+  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+
+//    EduTimeSdkInstance
+
+    if (call.method == "getPlatformVersion") {
+      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    } else {
+      result.notImplemented()
+    }
+  }
+
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+  }
+}
---
+++ example/android/app/src/main/kotlin/cz/edukids/edutime_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package cz.edukids.edutime_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
